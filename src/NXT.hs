7eb052bc197517b91492d4d2468218904bbdcb8c
jan <jan@varwig.org>
2008-09-06 20:16:36 +0200

    Basic Framework for implementing Commands created, playtone works
    darcs-hash:20080906181636-a6bf8-93cab6b3b953b232d62cd127fc053d28024e2ec1.gz

--- src/NXT.hs
+++ src/NXT.hs
@@ -1,25 +1,13 @@
 module NXT where
 	
-	import NXT.Codes
+	import NXT.Commands
 	import System.IO
-	import Data.Char
-	import Data.Word
-	import Data.Bits
-	
-	type Message = [Word8]
-	
-	-- | Add the BlueTooth length header in front of a message
-	btHeader :: [Word8] -> [Word8]
-	btHeader msg =  lengthLSB : lengthMSB : msg
-			where lengthLSB = (fromIntegral (length msg)) .&. 0xFF
-			      lengthMSB = (fromIntegral (length msg)) `shiftR` 8
-	
-	-- | Convert a message consisting of Bytes to a character-Stream required by hPutStr
-	bytesToChars :: [Word8] -> [Char]
-	bytesToChars bytes = fmap (chr . fromIntegral) bytes
+	import qualified Data.ByteString as B
 	
 	testloop = do
 		h <- openFile "/dev/tty.NXT-DevB-1" ReadWriteMode
 		hSetBuffering h NoBuffering
-		hPutStr h (bytesToChars (btHeader [0x00, 0x03, 0xf4, 0x01, 0xf4, 0x01]))
+		-- B.hPut h (B.pack [6,0,128,3,244,1,244,1])
+		playtone h 500 500
+		-- hFlush h
 		hClose h
--- src/NXT/Commands.hs
+++ src/NXT/Commands.hs
@@ -1,10 +1,81 @@
 module NXT.Commands where
 
-import NXT
+import NXT.Codes
+import System.IO
+import Data.Char
+import Data.Word
+import Data.Bits
+import qualified Data.ByteString as B
+
+type Message = B.ByteString
+
+------------------------------------------------------------------------------
+-- Helper functions
+------------------------------------------------------------------------------
+
+-- | Convert a String into a ByteString
+bsFromS :: String -> B.ByteString
+bsFromS chars = B.pack (fmap (fromIntegral . ord) chars)
+
+-- | Add the BlueTooth length header in front of a message
+btPack :: B.ByteString -> B.ByteString
+btPack msg = B.append (littleEndianPair (B.length msg)) msg
+-- TODO: Checken ob korrekt, evtl. verwirft das frühe Konvertieren 
+
+-- Convert an Integer to a little endian ByteString 
+littleEndianPair :: Int -> B.ByteString
+littleEndianPair i = B.pack [lsb, msb]
+			where lsb = fromIntegral (i .&. 0xFF)
+			      msb = fromIntegral (i `shiftR` 8)
+
+------------------------------------------------------------------------------
+-- Send and receive
+------------------------------------------------------------------------------
+
+-- | Send a command to the NXT
+send :: Handle			-- ^ IO Handle
+	-> Message		-- ^ The Command to send, not including the Commandmode
+	-> CommandMode		-- ^ The CommandMode
+	-> IO (Maybe Message)	-- ^ Will Always return Nothing
+send handle cmd mode = sendReceive handle cmd mode False
+
+-- | Receive Data from the NXT
+receive :: Handle
+	-> IO Message
+receive handle = B.hGetContents handle
+
+-- | Most Basic Communication function
+sendReceive :: Handle		-- ^ IO Handle
+	-> Message		-- ^ The Command to send, not including the Commandmode
+	-> CommandMode		-- ^ The CommandMode
+	-> Bool			-- ^ Indicate wether a reply is expected or not
+	-> IO (Maybe Message)	-- ^ Just The reply or Nothing	
+sendReceive handle cmd mode reply = do
+	B.hPut handle message
+	-- putStrLn (show (B.unpack cmd))
+	-- putStrLn (show (B.unpack cmdWithMode))
+	-- putStrLn (show (B.unpack message))
+	if reply
+		then do answer <- receive handle
+			return (Just answer)
+		else return Nothing
+	where message     = btPack cmdWithMode
+	      cmdWithMode = (commandType mode reply) `B.cons` cmd
+
+-- TODO Einheitliche Rückgabe, leere Listen?
+
+------------------------------------------------------------------------------
+-- Commands
+------------------------------------------------------------------------------
 
 -- STARTPROGRAM
 -- STOPPROGRAM
 -- PLAYSOUNDFILE
 
-playtone :: Bool -> Num -> Num -> Message
-playtone freq duration = 
+playtoneMsg :: Int -> Int -> Message
+playtoneMsg freq duration = 0x03 `B.cons` (B.append freqWord durWord)
+				where freqWord = littleEndianPair freq
+				      durWord  = littleEndianPair duration
+
+playtone :: Handle -> Int -> Int -> IO (Maybe Message)
+playtone handle freq duration = send handle (playtoneMsg freq duration) Direct
