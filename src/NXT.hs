5686722e9ec9c3694b79eb71b9c18b0bb7f36fdd
jan <jan@varwig.org>
2008-09-16 19:02:11 +0200

    Switch and Light Commands written
    darcs-hash:20080916170211-a6bf8-950f7fec867773ef3fee9ddc8cb341f61cdb01ef.gz

--- src/NXT.hs
+++ src/NXT.hs
@@ -1,35 +1,36 @@
 module NXT where
+
+import NXT.Commands
+import NXT.Codes
+import NXT.Helpers
+import NXT.Motor
+import NXT.Sensor
+import System.IO
+import System.Posix
+import qualified Data.ByteString as B
+
+testloop = do
+	h <- openFile "/dev/tty.NXT-DevB-1" ReadWriteMode
+	hSetBuffering h NoBuffering
+	-- B.hPut h (B.pack [6,0,128,3,244,1,244,1])
+	playtone h 500 500
+	-- sleep 2
+	-- setinputmode h Sensor4 Switch BooleanMode
+	setupDefaultSensors h
+	s <- getLight h
+	putStrLn (show s)
 	
-	import NXT.Commands
-	import NXT.Codes
-	import NXT.Helpers
-	import System.IO
-	import System.Posix
-	import NXT.Motor
-	import qualified Data.ByteString as B
-	
-	testloop = do
-		h <- openFile "/dev/tty.NXT-DevB-1" ReadWriteMode
-		hSetBuffering h NoBuffering
-		-- B.hPut h (B.pack [6,0,128,3,244,1,244,1])
-		playtone h 500 500
-		-- sleep 2
-		-- setinputmode h Sensor1 Switch BooleanMode
-		-- r <- getbatterylevel h
-		-- putStrLn (show r)
-		resetmotorposition h MotorA False
-		motorTurn h MotorA 30 360
 {-		-- putStrLn  (debugByteString (setoutputstateMsg MotorA 50 MotorOn MotorSpeed 50 Running 20))
-		setoutputstate h MotorA 30 [MotorOn, Brake, Regulated] MotorSpeed 0 Running 0
-		sleep 1
-		setoutputstate h MotorA (-30) [MotorOn, Brake, Regulated] MotorSpeed 0 Running 0
-		sleep 1
-		os <- getoutputstate h MotorA
-		putStrLn (show os)
-		setoutputstate h MotorA 0 [MotorOn, Brake, Regulated] MotorSpeed 0 RunStateIdle 0
-		sleep 1
-		setoutputstate h MotorA 0 [Coast] RegulationIdle 0 RunStateIdle 0
+	setoutputstate h MotorA 30 [MotorOn, Brake, Regulated] MotorSpeed 0 Running 0
+	sleep 1
+	setoutputstate h MotorA (-30) [MotorOn, Brake, Regulated] MotorSpeed 0 Running 0
+	sleep 1
+	os <- getoutputstate h MotorA
+	putStrLn (show os)
+	setoutputstate h MotorA 0 [MotorOn, Brake, Regulated] MotorSpeed 0 RunStateIdle 0
+	sleep 1
+	setoutputstate h MotorA 0 [Coast] RegulationIdle 0 RunStateIdle 0
 -}
-		sleep 1
-		hFlush h
-		hClose h
+	sleep 1
+	hFlush h
+	hClose h
--- src/NXT/Sensor.hs
+++ src/NXT/Sensor.hs
@@ -1,5 +1,34 @@
 module NXT.Sensor where
 
+import NXT.Commands
+import NXT.Codes
+import System.IO
+import Data.List
+import Data.Int
+import Data.Word
+
+setupDefaultSensors :: Handle -> IO ()
+setupDefaultSensors h = do
+	setinputmode h Sensor1 Switch      BooleanMode
+	setinputmode h Sensor2 SoundDb     RawMode
+	setinputmode h Sensor3 LightInactive RawMode
+	setinputmode h Sensor4 LowSpeed9V  BooleanMode	-- Ultrasonic Distance sensor
+
+getSwitch :: Handle -> IO (Bool)
+getSwitch h = do
+	iv <- getinputvalues h Sensor1
+	return (scaled iv > 0)
+
+
+
+
+
+
+getLight :: Handle -> IO (Float)
+getLight h = do
+	iv <- getinputvalues h Sensor3
+	return ((fromIntegral (normalized iv)) / 1023)
+	
 {-
 Sensor medium abstraction
   Doof dass die Sensoren nicht alle parallel ausgewählt werden können, sondern umgeschaltet werden müssen
